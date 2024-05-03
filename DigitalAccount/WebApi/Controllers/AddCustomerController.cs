ca8692f80994339d01553e4710587cde0b6c05f5
MASTER <brendonmauro@hotmail.com>
2024-05-02 23:49:00 -0300

    Feat: Add Customer Controller

---
+++ DigitalAccount/WebApi/Controllers/AddCustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddCustomer(AddCustomerInput input)
+        {
+
+        }
+    }
+}
